166da9068b858af1db480b327efe5866098c49c3
Fanhao Yu <fy229@cornell.edu>
2025-06-25 18:07:22 -0400

    test scroll fade in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h2>Introduction</h2>
       <p>
         Hi! I'm Fanhao, a computer science student at Cornell University. Hi!
-        I'm Fanhao, a computer science student at Cornell Univers
+        I'm Fanhao, a computer science student at Cornell University.
       </p>
       <div style={{ height: "150vh" }}></div> {/* Adds scroll */}
       <h1 className="text-4xl text-red-500 font-bold">Tailwind is working?</h1>
