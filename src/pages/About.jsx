0450e4a315798ddba873d0aafd074961ac73af70
Fanhao Yu <fy229@cornell.edu>
2025-07-14 17:04:09 -0400

    About page contents

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ export default function About() {
     <>
       <h1>About Page</h1>
       <p>About me</p>
+      <p>I am a sophomore at Cornell University studying computer science.</p>
       <div className="h-[150vh]"></div>
     </>
   );
